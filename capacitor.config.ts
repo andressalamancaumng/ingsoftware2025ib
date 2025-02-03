05e98c2975fcf35ea88a5fc3f2168cab2f0878f5
amsalamancaamirai <93617148+amsalamancaamirai@users.noreply.github.com>
2025-02-03 09:09:33 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Taller de prueba',
+  webDir: 'www'
+};
+
+export default config;
